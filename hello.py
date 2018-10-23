1e89dae7df5445aa4e55ff5894d66eae7c3d9df5
scott <scott.a.magnuson.19@dartmouth.edu>
2018-10-23 15:37:59 -0400

    basic api running

--- hello.py
+++ hello.py
@@ -5,10 +5,18 @@ app = Flask(__name__)
 def hello():
     if request.method == 'GET':
         #here we would need to get items
-        response = jsonify({
+        response = jsonify([{
         'id': '2332',
         'name': 'Barbra',
-        })
+        },
+        {
+        'id': '2352',
+        'name': 'James',
+        },
+        {
+        'id': '2336',
+        'name': 'Bob',
+        }])
         response.status_code = 200
     return response
 # main area
