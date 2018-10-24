47ae80550458cc3cc6d1e49d3f87c52149cca463
guohanting <guo.hanting@gmail.com>
2018-10-23 22:10:05 -0400

    adding post functionality

--- main.py
+++ main.py
@@ -3,29 +3,42 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/api/", methods =['GET'])
+@app.route("/api/", methods =['GET', 'POST'])
 def main():
     client1 = MongoClient('localhost', 27017)
     db = client1.exampledb
     people = db.people
     people_data = {
-        'title': 'Python and MongoDB',
-        'content': 'PyMongo is fun, you guys',
-        'author': 'Scott'
+        'Name': 'John Smith',
+        'Adult': True,
+        'Children': ['Johnny Smith, John Jr. Smith'],
+        'Email': 'jsmith@example.com'
+        'ID': '00001'
     }
     result = people.insert_one(people_data)
     print('One person: {0}'.format(result.inserted_id))
 
     if request.method == 'GET':
-        bills_post = people.find_one({'author': 'Scott'})
+        bills_post = people.find_one({'Name': 'John Smith'})
         #here we would need to get items
         response = jsonify([{
-        'id': str(bills_post['title']),
-        'name': str(bills_post['author']),
+        'Name': str(bills_post['Name']),
+        'Children': str(bills_post['Children']),
+        'Email': str(bills_post['Email']),
         }])
         response.status_code = 200
-    return response
     # main area
+
+    if request.method == 'POST':
+        request_json = request.get_json()
+        #here we would need to get items
+        value1 = request_json.get('id')
+        value2= request_json.get('name')
+        print(value1)
+        print(value2)
+        response = jsonify([{'status':'done'}])
+        response.status_code=200
+    return response
 
 if __name__ == "__main__":
     app.run()
